33b2fbd31e8958f04174c9b5d96720ba49052e4e
Diaz Matias Hernan <diaz.matiash@gmail.com>
2024-10-05 15:42:16 -0300

    Cosas para arreglar, pero funca por ahora, ID, Recurrencia de las Recoms, el update de libros

--- src/main/kotlin/ar/edu/unsam/algo3/repos/RepositorioLibros.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/repos/RepositorioLibros.kt
@@ -12,25 +12,25 @@ class RepositorioLibros : Repositorio<Libro>() {
     override fun buscarItems(patron: String) = items.filter { patron.lowercase() in it.titulo().lowercase()
             || patron.lowercase() in it.autor().apellido().lowercase() }
 
-    @Serializable
-    data class LibroPayload(
-        val id: Int,
-        val ediciones: Int,
-        val ventasSemanales: Int
-    )
+//    @Serializable
+//    data class LibroPayload(
+//        val id: Int,
+//        val ediciones: Int,
+//        val ventasSemanales: Int
+//    )
 
-    fun actualizarLibros() {
-        val payload = Json.decodeFromString<List<LibroPayload>>(service.getLibros())
-
-        payload.forEach { pay ->
-            val libro = items.find { it.id == pay.id }
-
-            if (libro != null) {
-                libro.editarEdiciones(pay.ediciones)
-                libro.editarVentasSemanales(pay.ventasSemanales)
-            } else {
-                throw Exception("El payload incluía una referencia a un ID inexistente.")
-            }
-        }
+   fun actualizarLibros() {
+//        val payload = Json.decodeFromString<List<LibroPayload>>(service.getLibros())
+//
+//        payload.forEach { pay ->
+//            val libro = items.find { it.id == pay.id }
+//
+//            if (libro != null) {
+//                libro.editarEdiciones(pay.ediciones)
+//                libro.editarVentasSemanales(pay.ventasSemanales)
+//            } else {
+//                throw Exception("El payload incluía una referencia a un ID inexistente.")
+//            }
+//        }
     }
 }
--- src/main/kotlin/ar/edu/unsam/algo3/repos/RepositorioRecomendaciones.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/repos/RepositorioRecomendaciones.kt
@@ -1,12 +1,13 @@
 package ar.edu.unsam.algo3.repos
 
 import ar.edu.unsam.algo3.Recomendacion
+import ar.edu.unsam.algo3.Usuario
 import ar.edu.unsam.algo3.model.recoms
 import org.springframework.stereotype.Component
 
 @Component
 class RepositorioRecomendaciones : Repositorio<Recomendacion>() {
-    override val items = recoms.toMutableSet()
+    override val items = mutableSetOf<Recomendacion>()
     override fun buscarItems(patron: String) = items.filter {
         patron.lowercase() == it.creador().apellido().lowercase() || patron.lowercase() in it.resegna().lowercase()
                 || it.libros().any { l -> patron.lowercase() in l.titulo().lowercase() }
