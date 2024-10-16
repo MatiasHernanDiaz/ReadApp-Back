9add057fd5ed42299f16142ada1fdb5df34c7a15
Theo <narmontastheo@gmail.com>
2024-10-16 20:59:28 -0300

    libro entregará datos proximamente

--- src/main/kotlin/ar/edu/unsam/algo3/controller/BookController.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/controller/BookController.kt
@@ -2,16 +2,14 @@ package ar.edu.unsam.algo3.controller
 
 import ar.edu.unsam.algo3.dto.toDTO
 import ar.edu.unsam.algo3.service.BookService
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @CrossOrigin(origins = ["http://localhost:4200"], methods = [RequestMethod.PUT, RequestMethod.POST, RequestMethod.GET])
 class BookController (val bookService: BookService){
 
     @GetMapping("/books")
-    fun recommendation() = bookService.getAllBook().map { it.toDTO() }
+    fun books(@RequestParam("text") text: String = "") =
+        bookService.getAllBook(text).map { it.toDTO() }
 
 }
