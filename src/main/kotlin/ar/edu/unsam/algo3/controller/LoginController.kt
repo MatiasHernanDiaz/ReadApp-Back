e05c83a98f568a57801a004c653b6b27d5c118ef
lautaCuellar <lautilh69@gmail.com>
2024-10-16 21:19:05 -0300

    arregle login, para que capture el error al no poder ingresar.

--- src/main/kotlin/ar/edu/unsam/algo3/controller/LoginController.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/controller/LoginController.kt
@@ -6,6 +6,7 @@ import ar.edu.unsam.algo3.dto.toDTO
 import ar.edu.unsam.algo3.service.LoginService
 import com.fasterxml.jackson.annotation.JsonFilter
 import org.springframework.boot.autoconfigure.ldap.embedded.EmbeddedLdapProperties.Credential
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -22,24 +23,36 @@ class LoginController(val loginService: LoginService) {
     fun getSignedUser(): SignedUserRes {
         val signedUser = loginService.getSignedUser()
 
-        if(signedUser === null) { return SignedUserRes(false, null) }
+        if(signedUser === null) { return SignedUserRes(false, null,"") }
 
-        return SignedUserRes(true, signedUser.toDTO())
+        return SignedUserRes(true, signedUser.toDTO(),"")
     }
 
     @PostMapping("/login")
-    fun login(@RequestBody credentials: CredentialsDTO) = SignedUserRes(
-        true, loginService.login(credentials.email, credentials.password).toDTO()
-    )
+    fun login(@RequestBody credentials: CredentialsDTO): ResponseEntity<SignedUserRes> {
+        val user = loginService.login(credentials.email, credentials.password)
+
+        return if (user != null) {
+            ResponseEntity.ok(SignedUserRes(true, user.toDTO(),"Login Existoso"))
+        } else {
+            ResponseEntity.status(401).body(SignedUserRes(false, null, "Credenciales Invalidas"))
+        }
+    }
+
+//    @PostMapping("/login")
+//    fun login(@RequestBody credentials: CredentialsDTO) = SignedUserRes(
+//        val user = loginService.login(credentials.email, credentials.password)
+//
+//    )
 
     @GetMapping("/logout")
     fun logout(): SignedUserRes {
         loginService.logout()
 
-        return SignedUserRes(false, null)
+        return SignedUserRes(false, null, "Usuario deslogueado")
     }
 }
 
 data class CredentialsDTO( val email: String, val password: String)
-data class SignedUserRes(val login: Boolean, val user: UserDTO?)
+data class SignedUserRes(val login: Boolean, val user: UserDTO?, val mensagge: String)
 
